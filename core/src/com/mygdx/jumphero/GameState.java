8057b3969849c8cc227e8ed8bea06028b93ac12c
Oscar Norman <oscar.norman95@gmail.com>
2022-01-03 09:50:31 +0100

    test

---
+++ core/src/com/mygdx/jumphero/GameState.java
@@ -0,0 +1,52 @@
+package com.mygdx.jumphero;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.jumphero.managers.GameStateManager;
+import com.mygdx.jumphero.managers.State;
+import com.mygdx.jumphero.renderers.OrthogonalTiledMapRendererBleeding;
+
+public class GameState extends State {
+
+    private final World world;
+    private final OrthogonalTiledMapRendererBleeding tiledMapRenderer;
+    private final TiledMap tiledMap;
+
+    public GameState(GameStateManager gsm) {
+        super(gsm);
+        world = new World(new Vector2(0, -9.8f), true);
+
+        tiledMap = new TmxMapLoader().load("core/assets/Map4.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRendererBleeding(tiledMap, 1f);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        world.step(dt, 6, 2);
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
+        JumpHero.viewport.apply();
+        tiledMapRenderer.setView( (OrthographicCamera) JumpHero.viewport.getCamera());
+        tiledMapRenderer.render();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
---
+++ core/src/com/mygdx/jumphero/managers/GameStateManager.java
@@ -0,0 +1,43 @@
+package com.mygdx.jumphero.managers;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+public class GameStateManager {
+    //a private Stack object for states
+    private Stack<State> states;
+
+    //constructor for GameStateManager
+    public GameStateManager(){
+        states = new Stack<State>();
+    }
+
+    //Method push to a new State onto this stack.
+    public void push(State state){
+        states.push(state);
+    }
+
+    //This method call returns the object at the top of this stack.
+    public void pop(){
+        states.pop();
+    }
+
+    //New method when we want to pop some state and instantly push a new state
+    public void set(State state){
+        states.pop();
+        states.push(state);
+
+    }
+
+    //dt is delta time , i.e. change in time between 2 renders
+    public void update(float dt){
+        states.peek().update(dt);
+    }
+    //Method to render SpriteBatch on the screen .It look at the top of the stack using peek method and
+//then it renders it on the screen
+    public void render (SpriteBatch sb){
+        states.peek().render(sb);
+    }
+
+}
