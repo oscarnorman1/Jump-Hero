49e2973832f559c1a70ff89f06f507c4311ce75c
Oscar Norman <oscar.norman95@gmail.com>
2022-02-12 00:41:42 +0100

    removed unnecessary code and assets

--- core/src/com/mygdx/jumphero/entities/Player.java
+++ core/src/com/mygdx/jumphero/entities/Player.java
@@ -15,14 +15,9 @@ import static com.mygdx.jumphero.util.Constants.PPM;
 public class Player extends Sprite {
 
     private World world;
-    private Texture texture;
     private Body player;
     private boolean facingRight;
     private boolean isJumping;
-
-    private Animation<TextureRegion> runRightAnimation; // Must declare frame type (TextureRegion)
-    private Animation<TextureRegion> runLeftAnimation; // Must declare frame type (TextureRegion)
-    private Texture animationSheet;
 
     private PlayerAnimations playerAnimations;
 
@@ -30,42 +25,11 @@ public class Player extends Sprite {
 
     public Player(World world) {
         this.world = world;
-        this.texture = new Texture("Player.jpg");
+        this.stateTime = 0f;
         player = createPlayer();
-        setTexture(texture);
         facingRight = true;
 
         this.playerAnimations = new PlayerAnimations();
-        this.animationSheet = new Texture(Gdx.files.internal("player_animation_sheet.png"));
-
-        TextureRegion[][] tmp = TextureRegion.split(animationSheet,
-                animationSheet.getWidth() / 8,
-                animationSheet.getHeight() / 8);
-
-        TextureRegion[] runRightFrames = new TextureRegion[8];
-        TextureRegion[] runLeftFrames = new TextureRegion[8];
-        int rightIndex = 0;
-        int leftIndex = 0;
-            for (int i = 0; i < 8; i++) {
-                runRightFrames[rightIndex++] = tmp[1][i];
-            }
-            for (int i = 0; i < 8; i++) {
-                runLeftFrames[leftIndex++] = tmp[1][i];
-            }
-            for (int i = 0; i < 8; i++) {
-                runLeftFrames[i].flip(true, false);
-            }
-            for (int i = 0; i < 8; i++) {
-                runRightFrames[i].flip(false, false);
-            }
-
-        this.runRightAnimation = new Animation<TextureRegion>(0.1f, runRightFrames);
-        this.runLeftAnimation = new Animation<TextureRegion>(0.1f, runLeftFrames);
-        this.stateTime = 0f;
-
-        // >
-        // <
-
     }
 
     @Override
@@ -74,15 +38,6 @@ public class Player extends Sprite {
 
         this.stateTime += Gdx.graphics.getDeltaTime();
 
-/*        TextureRegion currentRunRightFrame = runRightAnimation.getKeyFrame(stateTime, true);
-        TextureRegion currentRunLeftFrame = runLeftAnimation.getKeyFrame(stateTime, true);*/
-
-
-/*        if (isMoving && isFacingRight() && !isJumping) {
-            batch.draw(currentRunRightFrame, getBodyX(), getBodyY(), currentRunRightFrame.getRegionWidth() / PPM, currentRunRightFrame.getRegionHeight() / PPM);
-        } else if(isMoving && !isFacingRight() && !isJumping) {
-            batch.draw(currentRunLeftFrame, getBodyX(), getBodyY(), currentRunLeftFrame.getRegionWidth() / PPM, currentRunLeftFrame.getRegionHeight() / PPM);
-        }*/
         TextureRegion temp = playerAnimations.getTexureFrame(stateTime, facingRight, isMoving);
         batch.draw(temp, getBodyX(), getBodyY(), temp.getRegionWidth() / PPM, temp.getRegionHeight() / PPM);
 
@@ -154,11 +109,6 @@ public class Player extends Sprite {
 
     public void setFacingRight(boolean facingRight) {
         this.facingRight = facingRight;
-    }
-
-    @Override
-    public Texture getTexture() {
-        return texture;
     }
 
     public float getBodyX() {
--- core/src/com/mygdx/jumphero/states/GameState.java
+++ core/src/com/mygdx/jumphero/states/GameState.java
@@ -20,8 +20,6 @@ import static com.mygdx.jumphero.util.Constants.*;
 public class GameState extends State {
 
     private final World world;
-/*    private final OrthogonalTiledMapRendererBleeding tiledMapRenderer;
-    private final TiledMap tiledMap;*/
     private Box2DDebugRenderer b2dr;
     private Player player;
     private Array<Body> tmpBodies = new Array<Body>();
@@ -32,19 +30,13 @@ public class GameState extends State {
         this.world = new World(new Vector2(0, -14f), false);
         this.world.setContactListener(new B2dContactListener(this));
         b2dr = new Box2DDebugRenderer();
-        //b2dr.setDrawBodies(false);
+        b2dr.setDrawBodies(false);
         player = new Player(this.world);
-
 
         cam.setToOrtho(false, D_WIDTH / PPM, D_HEIGHT / PPM);
 
-/*        this.tiledMap = new TmxMapLoader().load("core/assets/Map4.tmx");
-        this.tiledMapRenderer = new OrthogonalTiledMapRendererBleeding(tiledMap, 1f / PPM);*/
-
         this.stageRenderer = new StageRenderer(this.world, this.cam, this.player);
 
-/*        TiledObjectUtil.parseTiledObjectLayer(world, tiledMap.getLayers().get("platform").getObjects(), "platform");
-        TiledObjectUtil.parseTiledObjectLayer(world, tiledMap.getLayers().get("walls").getObjects(), "walls");*/
         Gdx.input.setInputProcessor(new PlayerInputProccesor(this.player, this.gsm));
     }
 
@@ -67,8 +59,6 @@ public class GameState extends State {
         cam.update();
 
         JumpHero.viewport.apply();
-/*        tiledMapRenderer.setView( (OrthographicCamera) JumpHero.viewport.getCamera());
-        tiledMapRenderer.render();*/
         this.stageRenderer.render();
         b2dr.render(world, cam.combined);
         sb.setProjectionMatrix(cam.combined);
@@ -92,7 +82,6 @@ public class GameState extends State {
 
     @Override
     public void dispose() {
-        //tiledMapRenderer.dispose();
         b2dr.dispose();
         world.dispose();
     }
